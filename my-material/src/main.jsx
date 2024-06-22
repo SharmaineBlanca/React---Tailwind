205b7342714561ea8e3a7fe424c48b48cf2be4ff
Sharmaine_Blanca <114976961+SharmaineBlanca@users.noreply.github.com>
2024-06-22 23:18:46 +0800

    asd

--- my-material/src/main.jsx
+++ my-material/src/main.jsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
 
-     <NavbarSimple />
+   
       <Content />
       <ActivitiesTimeline />
       <CardWithLink />
